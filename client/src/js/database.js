bb3f16b908e877bae7e1c04a57bdd94d085a1bc8
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-12-01 20:57:07 -0600

    final cleanup

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -28,7 +28,6 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
-  console.log(result);
   return result.content;
 };
 
